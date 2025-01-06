2332a4f63d27cd5181077b99bf7b77e0960763ec
guchito <a_gaggero@hotmail.com>
2025-01-06 15:13:44 +0100

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const LINKS = {
+  github: "https://github.com/Guchito",
+  linkedin: "https://www.linkedin.com/in/agustingaggero/",
+  email: "mailto:agusgaggero@gmail.com"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
